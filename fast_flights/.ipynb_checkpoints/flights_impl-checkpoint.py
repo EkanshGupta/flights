4f9257eb4a9f379e269089fef0e61e5dcad15252
Ekansh Gupta <egupta8@login-phoenix-rh9-1.pace.gatech.edu>
2025-05-03 18:15:33 -0400

    updates

---
+++ fast_flights/.ipynb_checkpoints/flights_impl-checkpoint.py
@@ -0,0 +1,191 @@
+"""Typed implementation of flights_pb2.py"""
+
+import base64
+from typing import Any, List, TYPE_CHECKING, Literal, Union
+
+from . import flights_pb2 as PB
+from ._generated_enum import Airport
+
+if TYPE_CHECKING:
+    PB: Any
+
+class Bags:
+    """Represents bags
+    Args:
+        carryon (int): number of carryons.
+        checked (int): number of checked bags.
+    """
+
+    __slots__ = ("carryon", "checked")
+    carryon: int
+    checked: int
+
+    def __init__(
+        self,
+        *,
+        carryon: int,
+        checked: int
+    ):
+        self.carryon = carryon
+        self.checked = checked
+
+    def attach(self, info: PB.Info) -> None:  # type: ignore
+        info.bags.carryon = self.carryon
+        info.bags.checked = self.checked
+
+    def __repr__(self) -> str:
+        return (
+            f"Bags(carryon={self.carryon}, "
+            f"checked={self.checked})"
+        )
+
+class FlightData:
+    """Represents flight data.
+
+    Args:
+        date (str): Date.
+        from_airport (str): Departure (airport). Where from?
+        to_airport (str): Arrival (airport). Where to?
+    """
+
+    __slots__ = ("date", "from_airport", "to_airport")
+    date: str
+    from_airport: str
+    to_airport: str
+
+    def __init__(
+        self,
+        *,
+        date: str,
+        from_airport: Union[Airport, str],
+        to_airport: Union[Airport, str],
+    ):
+        self.date = date
+        self.from_airport = (
+            from_airport.value if isinstance(from_airport, Airport) else from_airport
+        )
+        self.to_airport = (
+            to_airport.value if isinstance(to_airport, Airport) else to_airport
+        )
+
+    def attach(self, info: PB.Info) -> None:  # type: ignore
+        data = info.data.add()
+        data.date = self.date
+        data.from_flight.airport = self.from_airport
+        data.to_flight.airport = self.to_airport
+
+    def __repr__(self) -> str:
+        return (
+            f"FlightData(date={self.date!r}, "
+            f"from_airport={self.from_airport}, "
+            f"to_airport={self.to_airport})"
+        )
+
+
+class Passengers:
+    def __init__(
+        self,
+        *,
+        adults: int = 0,
+        children: int = 0,
+        infants_in_seat: int = 0,
+        infants_on_lap: int = 0,
+    ):
+        assert (
+            sum((adults, children, infants_in_seat, infants_on_lap)) <= 9
+        ), "Too many passengers (> 9)"
+        assert (
+            infants_on_lap <= adults
+        ), "You must have at least one adult per infant on lap"
+
+        self.pb = []
+        self.pb += [PB.Passenger.ADULT for _ in range(adults)]
+        self.pb += [PB.Passenger.CHILD for _ in range(children)]
+        self.pb += [PB.Passenger.INFANT_IN_SEAT for _ in range(infants_in_seat)]
+        self.pb += [PB.Passenger.INFANT_ON_LAP for _ in range(infants_on_lap)]
+
+        self._data = (adults, children, infants_in_seat, infants_on_lap)
+
+    def attach(self, info: PB.Info) -> None:  # type: ignore
+        for p in self.pb:
+            info.passengers.append(p)
+
+    def __repr__(self) -> str:
+        return f"Passengers({self._data})"
+
+
+class TFSData:
+    """``?tfs=`` data. (internal)
+
+    Use `TFSData.from_interface` instead.
+    """
+
+    def __init__(
+        self,
+        *,
+        flight_data: List[FlightData],
+        bags: Bags,
+        seat: PB.Seat,  # type: ignore
+        trip: PB.Trip,  # type: ignore
+        passengers: Passengers,
+    ):
+        self.flight_data = flight_data
+        self.bags = bags
+        self.seat = seat
+        self.trip = trip
+        self.passengers = passengers
+
+    def pb(self) -> PB.Info:  # type: ignore
+        info = PB.Info()
+        info.seat = self.seat
+        info.trip = self.trip
+
+        self.passengers.attach(info)
+        self.bags.attach(info)
+
+        for fd in self.flight_data:
+            fd.attach(info)
+
+        return info
+
+    def to_string(self) -> bytes:
+        return self.pb().SerializeToString()
+
+    def as_b64(self) -> bytes:
+        return base64.b64encode(self.to_string())
+
+    @staticmethod
+    def from_interface(
+        *,
+        flight_data: List[FlightData],
+        bags: Bags,
+        trip: Literal["round-trip", "one-way", "multi-city"],
+        passengers: Passengers,
+        seat: Literal["economy", "premium-economy", "business", "first"],
+    ):
+        """Use ``?tfs=`` from an interface.
+
+        Args:
+            flight_data (list[FlightData]): Flight data as a list.
+            trip ("one-way" | "round-trip" | "multi-city"): Trip type.
+            passengers (Passengers): Passengers.
+            seat ("economy" | "premium-economy" | "business" | "first"): Seat.
+        """
+        trip_t = {
+            "round-trip": PB.Trip.ROUND_TRIP,
+            "one-way": PB.Trip.ONE_WAY,
+            "multi-city": PB.Trip.MULTI_CITY,
+        }[trip]
+        seat_t = {
+            "economy": PB.Seat.ECONOMY,
+            "premium-economy": PB.Seat.PREMIUM_ECONOMY,
+            "business": PB.Seat.BUSINESS,
+            "first": PB.Seat.FIRST,
+        }[seat]
+
+        return TFSData(
+            flight_data=flight_data, bags=bags, seat=seat_t, trip=trip_t, passengers=passengers
+        )
+
+    def __repr__(self) -> str:
+        return f"flight_data={self.flight_data},bags={self.bags},trip={self.trip},passengers={self.passengers},seat={self.seat})"
